f36605fd6a378c05191a440a6514b146595d6a55
M4rioV0 <marioff196@gmail.com>
2025-06-14 23:28:07 +0200

    Display keypresses

--- kilo.c
+++ kilo.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+#include <ctype.h>
+#include <stdio.h>
 
 struct termios orig_termios;
 
@@ -27,7 +29,13 @@ int main() {
 
     char c; 
     
-    while (read(STDIN_FILENO, &c,1) == 1&& c != 'q');
+    while (read(STDIN_FILENO, &c,1) == 1&& c != 'q'){
+        if(iscntrl(c)){
+            printf("%d\n", c);
+        }else{
+            printf("%d ('%c')\n", c, c);
+        }
+    }
 
     return 0;
 
