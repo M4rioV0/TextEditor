c5f997da9ec8cb64cceba96f2fad24cad7a41e6b
M4rioV0 <marioff196@gmail.com>
2025-06-15 16:30:47 +0200

    Turn off all output processing

--- kilo.c
+++ kilo.c
@@ -18,6 +18,7 @@ void enableRawMode() {
     atexit(disableRawMode);
     struct termios raw = orig_termios;
     raw.c_lflag &= ~(ICRNL | IXON);
+    raw.c_oflag &= ~(OPOST);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
@@ -32,9 +33,9 @@ int main() {
     
     while (read(STDIN_FILENO, &c,1) == 1&& c != 'q'){
         if(iscntrl(c)){
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         }else{
-            printf("%d ('%c')\n", c, c);
+            printf("%d ('%c')\r\n", c, c);
         }
     }
 
