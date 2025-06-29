c1081c8c86bfd43fcd23759e4d225a8928f19e42
M4rioV0 <marioff196@gmail.com>
2025-06-29 16:19:56 +0200

    error handling

--- kilo.c
+++ kilo.c
@@ -3,18 +3,25 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <errno.h>
 
 struct termios orig_termios;
 
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+
 void disableRawMode() {
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
 
 }
 
 void enableRawMode() {
     
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
     struct termios raw = orig_termios;
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK| ISTRIP | IXON );
@@ -24,16 +31,16 @@ void enableRawMode() {
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 
 }
 
 int main() {
 
     enableRawMode();
-    
-    while(1){ 
-        char c = '\0';
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+    while(1){
         read(STDIN_FILENO, &c,1);
         if(iscntrl(c)){
             printf("%d\r\n", c);
