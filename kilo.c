f426184646762f8d95cf49c0666b5f0d656b6fad
M4rioV0 <marioff196@gmail.com>
2025-06-30 02:50:19 +0200

    Window size, the easy way

--- kilo.c
+++ kilo.c
@@ -4,6 +4,7 @@
 #include <termios.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <sys/ioctl.h>
 #include <stdio.h>
 #include <errno.h>
 
@@ -61,6 +62,19 @@ char editorReadKey() {
         if (nread == -1 && errno != EAGAIN) die("read");
     }
     return c;
+
+}
+
+int getWindowSize(int *rows, int *cols){
+    struct winsize ws;
+    
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        return -1;
+    }else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
 
 }
 
